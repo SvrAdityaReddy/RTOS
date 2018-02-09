692d8f16862109d7e50bb48c5230f7ffee281c4c
SvrAdityaReddy <Aditya.seelapureddy@yahoo.com>
2018-02-09 22:04:38 +0530

    updated code

--- Assignment_4/message_queues/message_queue_server.c
+++ Assignment_4/message_queues/message_queue_server.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <limits.h>
 #include <fcntl.h>           
 #include <sys/stat.h>        
 #include <mqueue.h>
@@ -7,10 +9,87 @@
 #define MAX_MESSAGES 10
 #define MAX_MSG_SIZE 256
 
+int evaluate(char *buffer) {
+    int length_buffer=strlen(buffer);
+    char *r = strndup(buffer,length_buffer);
+    char *tok = r;
+    /*
+    operator array
+    '~' refers to invalid operator
+    */
+    char *delimiter[5]={"+","-","*","/","~"};
+    char delimiter_position=0;
+    int a,b,i,result;
+    int values[2]={0,0};
+    i=0;
+    // while(tok != NULL) {
+    //     strsep(&end, delimiter[delimiter_position]);
+    //     printf("%s\n",tok);
+    //     if(strncmp(tok,buffer,length_buffer)==0) {
+    //         delimiter_position=delimiter_position+1;
+    //         if(delimiter_position==5) {
+    //             break;
+    //         }
+    //     }
+    //     else {
+    //         values[i]=atoi(tok);
+    //         i=i+1;
+    //     }
+    //     tok = end;
+    // }
+    // printf("%d %d\n",values[0],values[1]);
+    // printf("%d\n",delimiter_position);
+    // free(r);
+    char *token = strtok(r, delimiter[delimiter_position]);
+   
+    // Keep printing tokens while one of the
+    // delimiters present in str[].
+    while (token!=NULL) {
+        // printf("%s\n", token);
+        if(strncmp(token,buffer,length_buffer)==0) {
+            delimiter_position=delimiter_position+1;
+            token = strtok(r, delimiter[delimiter_position]);
+            if(delimiter_position==5) {
+                break;
+            }
+        }
+        else {
+            values[i]=atoi(token);
+            i=i+1;
+            token = strtok(NULL, delimiter[delimiter_position]);
+        }
+    }
+    // printf("%d\n",delimiter_position);
+    if(delimiter[delimiter_position]=="+") {
+        result=values[0]+values[1];
+    }
+    else {
+        if(delimiter[delimiter_position]=="-") {
+            result=values[0]-values[1];
+        }
+        else {
+            if(delimiter[delimiter_position]=="*") {
+                result=values[0]*values[1];
+            }
+            else {
+                if(delimiter[delimiter_position]=="/") {
+                    result=values[0]/values[1];
+                }
+                else {
+                    // Invalid Operator                    
+                    result=INT_MIN;
+                }
+            }
+        }
+    }
+    return result;
+}
+
 int main(int argc, char *argv[]) {
     mqd_t mqd;
     struct mq_attr attr;
     char *buffer;
+    int i=0,result=0;
     attr.mq_flags = 0; 
     attr.mq_maxmsg = MAX_MESSAGES; 
     attr.mq_msgsize = MAX_MSG_SIZE; 
@@ -29,8 +108,15 @@ int main(int argc, char *argv[]) {
             printf("Error in receiving message\n");
             return 0;
         }
-        // Expression evaluation
         printf("Data received from Client: %s\n",buffer);
+        // Simple expression evaluation
+        result=evaluate(buffer);
+        if(result==INT_MIN) {
+            printf("Invalid Operator\n");
+        }
+        else {
+            printf("Expression %s is evaluated to %d\n",buffer,result);
+        }
     }
     free(buffer);
     mq_unlink("/server_queue");
