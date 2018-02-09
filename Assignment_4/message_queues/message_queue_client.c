72870545c9998ccef25f58467465ad6a0abfc346
SvrAdityaReddy <Aditya.seelapureddy@yahoo.com>
2018-02-09 14:40:01 +0530

    updated message queue code

--- Assignment_4/message_queues/message_queue_client.c
+++ Assignment_4/message_queues/message_queue_client.c
@@ -7,13 +7,15 @@
 
 int main(int argc, char *argv[]) {
     mqd_t mqd;
-    mqd=mq_open("server_queue",O_WRONLY);
-    if(mqd == (mqd_t) -1) {
-        printf("Error in creating server's message queue\n");
+    mqd=mq_open("/server_queue",O_WRONLY);
+    if(mqd==(mqd_t)-1) {
+        printf("Error in opening server's message queue\n");
         return 0;
     }
     if(mq_send(mqd,argv[1],strlen(argv[1]),0)==-1) {
+        mq_close(mqd);
         printf("Error in sending message\n");
     }
+    mq_close(mqd);
     return 0;
 }
--- Assignment_4/message_queues/message_queue_server.c
+++ Assignment_4/message_queues/message_queue_server.c
@@ -4,27 +4,35 @@
 #include <sys/stat.h>        
 #include <mqueue.h>
 
-struct mq_attr;
+#define MAX_MESSAGES 10
+#define MAX_MSG_SIZE 256
 
 int main(int argc, char *argv[]) {
     mqd_t mqd;
     struct mq_attr attr;
-    int priority;
-    void *buffer;
-    mqd=mq_open("/server_queue",O_CREAT |O_RDONLY ,O_RDWR);
+    char *buffer;
+    attr.mq_flags = 0; 
+    attr.mq_maxmsg = MAX_MESSAGES; 
+    attr.mq_msgsize = MAX_MSG_SIZE; 
+    attr.mq_curmsgs = 0;
+    mq_unlink("/server_queue");
+    mqd=mq_open("/server_queue",O_CREAT|O_RDWR, O_RDWR,&attr);
     if(mqd == (mqd_t) -1) {
         printf("Error in creating server's message queue\n");
         return 0;
     }
-    if(mq_getattr(mqd,&attr)==-1) {
-        printf("Error in getting message queue attributes\n");
-        return 0;
+    buffer=malloc(sizeof(char)*attr.mq_msgsize);    
+    while(1) {
+        if(mq_receive(mqd,buffer, attr.mq_msgsize, NULL)==-1) {
+            mq_unlink("/server_queue");
+            free(buffer);
+            printf("Error in receiving message\n");
+            return 0;
+        }
+        // Expression evaluation
+        printf("Data received from Client: %s\n",buffer);
     }
-    buffer=malloc(attr.mq_msgsize);
-    if(mq_receive(mqd,buffer, attr.mq_msgsize, &priority)==-1) {
-        printf("Error in receiving message\n");
-        return 0;
-    }
-    // printf(buffer);
+    free(buffer);
+    mq_unlink("/server_queue");
     return 0;
 }
